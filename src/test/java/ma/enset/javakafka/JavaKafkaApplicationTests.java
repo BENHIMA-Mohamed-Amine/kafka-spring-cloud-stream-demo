049bbaad150ae7981d735a5cfa4bcda3200cc3b8
BENHIMA Mohamed-amine <benhima.mohamed.amine@gmail.com>
2025-10-27 14:59:13 +0100

    first commit

---
+++ src/test/java/ma/enset/javakafka/JavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.javakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
